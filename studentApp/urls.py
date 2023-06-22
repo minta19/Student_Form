6b0b2f425f6b3eb1ea0fdf82a538f0e37d7e5771
minta <mintajose19@gmail.com>
2023-06-22 17:31:17 +0530

    Added new feature

--- studentApp/urls.py
+++ studentApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
+from .views import StudentView
 urlpatterns=[
-    path('create/',views.CreateStudent,name="createstudent")
+    path('create/',views.CreateStudent,name="createstudent"),
+    path('studview/',views.StudentView.as_view(),name='stud_list')
 ]
--- studentApp/views.py
+++ studentApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import StudentCreate
 from .models import Student
+from django.views.generic import ListView
 # Create your views here.
 
 def CreateStudent(request):
@@ -12,4 +13,9 @@ def CreateStudent(request):
             return redirect('createstudent')
     context={"form":form}
     return render(request,'studentApp/studtemp.html',context) 
+
+class StudentView(ListView):
+    template_name = 'studentApp/stuview.html'
+    queryset=Student.objects.all()
+    context_object_name="allstudent"
 
